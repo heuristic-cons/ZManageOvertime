f23af4a94444a5721cded0b3de2e53168d130988
Alex <alexandros.kontopoulos@heuristic-cons.com>
2019-06-26 11:42:43 +0100

    Error handling fixed

--- webapp/controller/Add.controller.js
+++ webapp/controller/Add.controller.js
@@ -3,9 +3,10 @@ sap.ui.define([
 	"sap/ui/core/routing/History",
 	"sap/m/MessagePopover",
 	"sap/m/MessageItem",
-	"sap/m/MessageToast"	
+	"sap/ui/model/json/JSONModel",
+	"sap/m/MessageToast"
 ], function(BaseController, History, MessagePopover, MessageItem, MessageToast) {
-	"use strict"; 
+	"use strict";
 
 	return BaseController.extend("com.getronics.hr.ZManageOvertime.controller.Add", {
 
@@ -13,12 +14,11 @@ sap.ui.define([
 		/* lifecycle methods                                           */
 		/* =========================================================== */
 
-
 		/**
 		 * Called when the add controller is instantiated.
 		 * @public
 		 */
-		onInit: function() { 
+		onInit: function() {
 			// Register to the add route matched
 			this.getRouter().getRoute("add").attachPatternMatched(this._onRouteMatched, this);
 			// this.getView().byId("page").bindElement("/OT_request('4')");
@@ -35,33 +35,33 @@ sap.ui.define([
 		 * @public
 		 */
 		onSave: function() {
-		   var sValue = "";
-		   var oModel = this.getView().getModel();
-           var path = this._oContext.sPath + "/ZaatypeText";		        
-           oModel.setProperty( path, sValue);			
+			var sValue = "";
+			var oModel = this.getView().getModel();
+			var path = this._oContext.sPath + "/ZaatypeText";
+			oModel.setProperty(path, sValue);
 			this.getModel().submitChanges();
 		},
 
-        onSubmit: function() {
-        	
-        },
+		onSubmit: function() {
 
-       onAttSel: function(oEvent) {
+		},
+
+		onAttSel: function(oEvent) {
 			var oValidatedComboBox = oEvent.getSource(),
 				sSelectedKey = oValidatedComboBox.getSelectedKey(),
 				sValue = oValidatedComboBox.getValue();
-				var oModel = this.getView().getModel();
-				var path = this._oContext.sPath + "/ZzAttType";
-		        oModel.setProperty( path, sSelectedKey);
-		        path = "/OT_typesSet('" + sSelectedKey + "')/OtHelp";
-		        
-		        sValue = oModel.getProperty( path ); 
-                path = this._oContext.sPath + "/ZaatypeText";		        
-                oModel.setProperty( path, sValue);
-       },
-       customFooterContent: function(oEvent) {
-       	
-       }, 
+			var oModel = this.getView().getModel();
+			var path = this._oContext.sPath + "/ZzAttType";
+			oModel.setProperty(path, sSelectedKey);
+			path = "/OT_typesSet('" + sSelectedKey + "')/OtHelp";
+
+			sValue = oModel.getProperty(path);
+			path = this._oContext.sPath + "/ZaatypeText";
+			oModel.setProperty(path, sValue);
+		},
+		customFooterContent: function(oEvent) {
+
+		},
 		/* =========================================================== */
 		/* event handlers                                              */
 		/* =========================================================== */
@@ -71,57 +71,106 @@ sap.ui.define([
 			var oModel = this.getModel();
 			oModel.metadataLoaded().then(this._onMetadataLoaded.bind(this));
 		},
-		_onMetadataLoaded: function () {
+		_onMetadataLoaded: function() {
 			// create default properties
 			var oProperties = {
-				Zaction            : "SAVE"
+				Zaction: "SAVE"
 			};
 			// create new entry in the model
 			var oModel = this.getModel();
 			this._oContext = oModel.createEntry("/OT_requestSet", {
 				properties: oProperties,
-                success: this._onCreateSuccess.bind(this)				
+				success: this._onCreateSuccess.bind(this)
 			});
-			
+
 			// bind the view to the new entry
 			this.getView().setBindingContext(this._oContext);
 		},
-        _onCreateSuccess: function(oRequest){
-        	var sId = oRequest.Zdocnr;
-        	if (sId !== "0000000000"){ 
-        	this.getRouter().navTo("object", {objectId: sId},true );
-        	}
-        	else {
-        	 var oModel = this.getView().getModel();
-			 var path = this._oContext.sPath + "/Error1";
-			 var error = oModel.getProperty(path);
-			 var oMessageTemplate = new MessageItem({
-					type: 'Error',
-					title: 'Error Message',
+		_onCreateSuccess: function(oRequest) {
+			var sId = oRequest.Zdocnr;
+			if (sId !== "0000000000") {
+				this.getRouter().navTo("object", {
+					objectId: sId
+				}, true);
+			} else {
+				var oModel = this.getView().getModel();
+				var path = this._oContext.sPath + "/Error1";
+				var error = oModel.getProperty(path);
+				var oMessageTemplate = new MessageItem({
+					type: "Error",
+					title: "Error Message",
 					activeTitle: "",
 					description: error,
-					subtitle: '',
+					subtitle: "",
 					counter: 1
 				});
 
-	var oMessagePopover = new MessagePopover({
-		items: {
-			path: '/',
-			template: oMessageTemplate
+			}
 		},
-		activeTitlePress: function () {
-			MessageToast.show('Active title is pressed');
-		}
-	});			 
-        	}
-        },
+		onhandleMessage: function(oEvent) {
+			var oModel = this.getView().getModel();
+			var path = this._oContext.sPath + "/Error1";
+			var error = oModel.getProperty(path);
+
+			var oMessageTemplate = new MessageItem({
+				type: "{type}",
+				title: "{title}",
+				activeTitle: "{active}",
+				description: "{description}",
+				subtitle: "{subtitle}",
+				counter: "{counter}"
+			});
+			var count = 0;
+			var index = 0;
+			if (error !== "") {
+				count = count + 1;
+				
+				var oMessage = [{
+					type: "Error",
+					title: "Error message",
+					active: true,
+					description: error,
+					subtitle: "",
+					counter: count
+				}];
+			}
+			path = this._oContext.sPath + "/Error2";
+			error = oModel.getProperty(path);
+			if (error !== "") {
+				count = count + 1;
+				index = index + 1;
+				 oMessage.push(
+				 {
+					type: "Error",
+					title: "Error message",
+					active: true,
+					description: error,
+					subtitle: "",
+					counter: count
+				});
+			}			
+			
+			var oMsgModel = new sap.ui.model.json.JSONModel();
+			oMsgModel.setData(oMessage);
+			var oMessagePopover = new MessagePopover({
+				items: {
+					path: "/",
+					template: oMessageTemplate
+				},
+				activeTitlePress: function() {
+					MessageToast.show("Active title is pressed");
+				}
+			});
+			oMessagePopover.setModel(oMsgModel);
+			oMessagePopover.toggle(oEvent.getSource());
+		},
 		/**
 		 * Event handler for navigating back.
 		 * It checks if there is a history entry. If yes, history.go(-1) will happen.
 		 * If not, it will replace the current entry of the browser history with the worklist route.
 		 * @public
 		 */
-		onNavBack : function() {
+		onNavBack: function() {
 			// discard new product from model.
 			this.getModel().deleteCreatedEntry(this._oContext);
 
